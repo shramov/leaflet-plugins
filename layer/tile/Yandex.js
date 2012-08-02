b4d8e70952ea00f09a412ef957d83b2193b7a062
Pavel Shramov <psha@void.psha.org.ru>
2012-08-02 22:05:44 +0400

    yandex, google: Fix zIndex issues (fixes #29 #30)

--- layer/tile/Yandex.js
+++ layer/tile/Yandex.js
@@ -74,6 +74,7 @@ L.Yandex = L.Class.extend({
 		if (!this._container) {
 			this._container = L.DomUtil.create('div', 'leaflet-yandex-layer leaflet-top leaflet-left');
 			this._container.id = "_YMapContainer_" + L.Util.stamp(this);
+			this._container.style.zIndex = "auto";
 		}
 
 		if (this.options.overlay) {
