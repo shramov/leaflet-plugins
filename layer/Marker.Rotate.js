df19d88118055b219fa66371fc31fa7600307d00
Pavel Shramov <psha@void.psha.org.ru>
2012-08-02 20:36:42 +0400

    marker.rotate: Update to 0.4 api

--- layer/Marker.Rotate.js
+++ layer/Marker.Rotate.js
@@ -2,19 +2,19 @@
  * Based on comments by @runanet and @coomsie 
  * https://github.com/CloudMade/Leaflet/issues/386
  *
- * Wrapping function is needed to preserve L.Marker._reset function
+ * Wrapping function is needed to preserve L.Marker.update function
  */
 (function () {
-var _old_reset = L.Marker.prototype._reset;
+var _old_update = L.Marker.prototype.update;
 L.Marker.include({
-	_reset: function() {
+	update: function() {
 		this._icon.style[L.DomUtil.TRANSFORM] = "";
-		_old_reset.apply(this, []);
+		_old_update.apply(this, []);
 
 		if (this.options.iconAngle) {
 			var a = this.options.icon.options.iconAnchor;
 			var s = this.options.icon.options.iconSize;
-			a = s.divideBy(2)._subtract(a);
+			a = L.point(s).divideBy(2)._subtract(a);
 			var transform = '';
 			transform += ' translate(' + -a.x + 'px, ' + -a.y + 'px)';
 			transform += ' rotate(' + this.options.iconAngle + 'deg)';
@@ -26,7 +26,7 @@ L.Marker.include({
 	setIconAngle: function (iconAngle) {
 		this.options.iconAngle = iconAngle;
 
-		if (this._map) this._reset();
+		if (this._map) this.update();
 	}
 });
 }());
