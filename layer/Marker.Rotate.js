487055b568a4f35eccd0560735d81846984f64a5
William McRae <william.mcrae@gmail.com>
2013-05-13 14:32:55 +1000

    handling rotation when zooming

--- layer/Marker.Rotate.js
+++ layer/Marker.Rotate.js
@@ -5,16 +5,32 @@
  * Wrapping function is needed to preserve L.Marker.update function
  */
 (function () {
-    var _old_update = L.Marker.prototype.update;
+    var _old__setPos = L.Marker.prototype._setPos;
     L.Marker.include({
-        update: function() {
+        _updateImg: function(i, a, s) {
+            a = L.point(s).divideBy(2)._subtract(L.point(a));
+            var transform = '';
+            transform += ' translate(' + -a.x + 'px, ' + -a.y + 'px)';
+            transform += ' rotate(' + this.options.iconAngle + 'deg)';
+            transform += ' translate(' + a.x + 'px, ' + a.y + 'px)';
+            i.style[L.DomUtil.TRANSFORM] += transform;
+        },
+
+        setIconAngle: function (iconAngle) {
+            this.options.iconAngle = iconAngle;
+
+            if (this._map) this.update();
+        },
+
+        _setPos: function (pos) {
             if (this._icon) {
                 this._icon.style[L.DomUtil.TRANSFORM] = "";
             }
             if (this._shadow) {
                 this._shadow.style[L.DomUtil.TRANSFORM] = "";
             }
-            _old_update.apply(this, []);
+
+            _old__setPos.apply(this,[pos]);
 
             if (this.options.iconAngle) {
                 var a = this.options.icon.options.iconAnchor;
@@ -32,22 +48,6 @@
                     this._updateImg(i, a, s);
                 }
 
-            }
-        },
-
-        _updateImg: function(i, a, s) {
-            a = L.point(s).divideBy(2)._subtract(L.point(a));
-            var transform = '';
-            transform += ' translate(' + -a.x + 'px, ' + -a.y + 'px)';
-            transform += ' rotate(' + this.options.iconAngle + 'deg)';
-            transform += ' translate(' + a.x + 'px, ' + a.y + 'px)';
-            i.style[L.DomUtil.TRANSFORM] += transform;
-        },
-
-        setIconAngle: function (iconAngle) {
-            this.options.iconAngle = iconAngle;
-
-            if (this._map) this.update();
-        }
+            }    }
     });
 }());
