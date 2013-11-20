42a5a57f19b07391e8b95150459a247ddcc919e1
opie <owen.peterson@gmail.com>
2013-11-19 20:00:38 -0500

    Added support for KML GroundOverlay tag
    -parseGroundOverlay function added for KML GroundOverlay tag
    -parseXML now gets passed 'options' so that it can be passed onto parseGroudOverlay (see function header comments for application options)
    -loadXML modified to support blob:/ style URLs - this is very useful when you already have the KML document in memory

--- layer/vector/KML.js
+++ layer/vector/KML.js
@@ -18,7 +18,10 @@ L.KML = L.FeatureGroup.extend({
 	loadXML: function(url, cb, options, async) {
 		if (async == undefined) async = this.options.async;
 		if (options == undefined) options = this.options;
-
+		if (url.substr(0,5).toLowerCase() == 'blob:')	{	options['urlIsBlob'] = true;	
+															options['kmlBlob'] = url; // store this URL reference for later memory clean-up - not yet implemented
+														}
+		
 		var req = new window.XMLHttpRequest();
 		req.open('GET', url, async);
 		try {
@@ -26,7 +29,10 @@ L.KML = L.FeatureGroup.extend({
 		} catch(e) {}
 		req.onreadystatechange = function() {
 			if (req.readyState != 4) return;
-			if(req.status == 200) cb(req.responseXML, options);
+			if (options.urlIsBlob == true)
+			{	cb(req.responseXML, options);	}
+			else
+			{	if(req.status == 200) cb(req.responseXML, options);		}
 		};
 		req.send(null);
 	},
@@ -38,11 +44,10 @@ L.KML = L.FeatureGroup.extend({
 	},
 
 	_addKML: function(xml, options) {
-		var layers = L.KML.parseKML(xml);
+		var layers = L.KML.parseKML(xml, options);
 		if (!layers || !layers.length) return;
 		for (var i = 0; i < layers.length; i++)
-		{
-			this.fire('addlayer', {
+		{	this.fire('addlayer', {
 				layer: layers[i]
 			});
 			this.addLayer(layers[i]);
@@ -56,7 +61,7 @@ L.KML = L.FeatureGroup.extend({
 
 L.Util.extend(L.KML, {
 
-	parseKML: function (xml) {
+	parseKML: function (xml, options) {
 		var style = this.parseStyle(xml);
 		this.parseStyleMap(xml, style);
 		var el = xml.getElementsByTagName("Folder");
@@ -72,6 +77,11 @@ L.Util.extend(L.KML, {
 			l = this.parsePlacemark(el[j], xml, style);
 			if (l) { layers.push(l); }
 		}
+		el = xml.getElementsByTagName('GroundOverlay');
+		for (var j = 0; j < el.length; j++) {
+			l = this.parseGroundOverlay(el[j], style, options);
+			if (l) { layers.push(l); }
+		}		
 		return layers;
 	},
 
@@ -317,6 +327,146 @@ L.Util.extend(L.KML, {
 			coords.push(new L.LatLng(ll[1], ll[0]));
 		}
 		return coords;
+	},
+	
+	//note that 'options' is passed through to parseKML so that it can reach parseGroundOverlay which uses it
+	//to look for customizations to the groundOverlay, specficially if a bounding box should be drawn with a PopUp
+	// * New options that can be passed through for processing of GroundOverlay tags are
+	// options['imageOverlayBoundingBoxCreatePopUp'] = true|false
+	// options['imageOverlayBoundingBoxDrawOptions'] = {L.PopUp.options}   example:
+	//		options['imageOverlayBoundingBoxDrawOptions'] = {stroke: true, weight: 2, fillOpacity: 0.05,clickable: true}
+	parseGroundOverlay: function (overlay, style, options) {
+		var i, j, el;
+		var blobURLs=[];  //keeps track of blob URLs created by KMZ processor for contained image files, these need to be release when the object is removed from the map
+
+		// Parse out the Icon, href and Color tags (for opacity)
+		var overlayOptions = _parse(overlay);  
+		
+		//Get the co-ordinates and create latLonBox for this image
+		var south, west, north, east, rotation, latLonBox;
+		el = overlay.getElementsByTagName('LatLonBox');
+		for (i = 0; i < el.length; i++) 
+		{	for (j = 0; j < el[i].childNodes.length; j++) 
+			{
+				if (el[i].childNodes[j].tagName == 'north')
+				{	north = el[i].childNodes[j].textContent;	} 
+				else if (el[i].childNodes[j].tagName == 'south')
+				{	south = el[i].childNodes[j].textContent;	}
+				else if (el[i].childNodes[j].tagName == 'east')
+				{	east = el[i].childNodes[j].textContent;	}
+				else if (el[i].childNodes[j].tagName == 'west')
+				{	west = el[i].childNodes[j].textContent;	}
+//				else if (el[i].childNodes[j].tagName == 'rotation')
+//				{	//Possibly add support to rotate the image outside of Leaflet, look into how the other plugins are doing it	}
+			}
+		}
+		latLonBox =  new L.LatLngBounds([ new L.LatLng(south, west), new L.LatLng(north, east) ]);
+		
+		//Create the ImageOverlay
+		var imgOverlay = new L.ImageOverlay( overlayOptions.href, latLonBox, new Array().concat(options, overlayOptions)  );
+		
+		//Extend the ImageOverlay Object to have a getBounds method so that we can call L.Map.fitBounds on it later
+		L.Util.extend(imgOverlay, {getBounds: function() {   return this._bounds; } });
+		
+		
+		var layers = [];
+		if (imgOverlay) 
+		{ layers.push(imgOverlay); }
+
+		if (!layers.length) 
+		{	return;	}
+		
+		var layer = new L.FeatureGroup(layers);	
+		
+		//Since LeafletJS doesn't provide a means of having a PopUp box on a ImageOverlay
+		//this option - passed through at L.KMZ creationg allows for a L.Polygon to be created
+		//With various formatting options passed along (set weight, opacity, clickable) a
+		//L.PopUp can be bound to the surrounding areas, alas you have to click on the box borders sometimes
+		//but not at other times which is very odd.
+		if (options.imageOverlayBoundingBoxCreatePopUp==true)
+		{	var name, descr = "";
+			
+			el = overlay.getElementsByTagName('name');
+			if (el.length) 
+			{	name = el[0].childNodes[0].nodeValue;	}
+			
+			el = overlay.getElementsByTagName('description');
+			for (i = 0; i < el.length; i++) {
+				for (j = 0; j < el[i].childNodes.length; j++) {
+					descr = descr + el[i].childNodes[j].nodeValue;
+				}
+			}
+			
+			var imageOverlayBoundingBoxDrawOptions = (options.imageOverlayBoundingBoxDrawOptions) ? options.imageOverlayBoundingBoxDrawOptions : {}; 
+			var hiddenPoly = new L.Polygon(	[ new L.LatLng(south, west), new L.LatLng(south, east), 
+			                               	  new L.LatLng(north, east), new L.LatLng(north, west) ], 
+											imageOverlayBoundingBoxDrawOptions);
+			if (name) 
+			{	hiddenPoly.bindPopup('<h2>' + name + '</h2>' + descr);	}
+			
+			layer.addLayer(hiddenPoly);
+		}
+
+		if (blobURLs.length > 0)
+		{	//Setup a listener to remove the blobs from memory by their blob:/ URLs when this feature group is removed from the map
+			//Todo: This doesn't appear to fire correctly though, needs some investigation as to why.
+			layer.on(	'layerremove', 	
+					function()	
+					{	console.log('In Blob onLayerRemove Event Handler');
+					
+						// Remove the blobs for each image loaded as a blob by L.KMZ
+						blobURLs.forEach( 	function(blobURL) 	
+											{	URL.revokeObjectURL(blobURL);
+												console.log('Removed Blob URL:', blobURL);
+											} ); 
+						
+						URL.revokeObjectURL(this.options['kmlBlob']);  // Remove the blob containing the KML document
+						
+					}, this);
+		}
+		
+		return layer;
+
+		
+		//Utility inner function to parse out the Icon, href and Color tags
+		//Also keeps track of any 'blob:' URLs it runs into for future memory release when the layer is removed from the map
+		function _parse(xml) {
+			var options = {};
+			for (var i = 0; i < xml.childNodes.length; i++) 
+			{	
+				var e = xml.childNodes[i];
+				var key = e.tagName;
+				if (key)
+				{
+					var value = e.childNodes[0].nodeValue;
+					if (key === 'color') {
+						options.opacity = parseInt(value.substring(0, 2), 16) / 255.0;
+						options.color = "#" + value.substring(6, 8) + value.substring(4, 6) + value.substring(2, 4);
+					} 
+					else if (key === 'Icon') 
+					{	ioptions = _parse(e);
+						if (ioptions.href) 
+						{	options.href = ioptions.href;	}
+					} 
+					else if (key === 'href') 
+					{	if (value.substr(0,5).toLowerCase() == 'blob:')
+						{	blobURLs.push(value);		  //keep track of this blob URL to release the memory when this layer is removed from the map
+							options.href = value;
+						}
+						else if (value.substr(0,5).toLowerCase() == 'http:')
+						{	options.href = value;	}
+//						else if ( (options['baseURL']) && (options['baseURL'].length > 0))
+//						{	var baseUrl = options['baseURL'].substr(0, options['baseURL'].lastIndexOf('/') + 1);
+//							options.href = baseUrl + value;	
+//							console.log('I SET THE BASE URL:', options.href);
+//						}
+						else	
+						{	options.href = value;	}
+					}
+				}
+			}
+			return options;
+		};
 	}
 
 });
@@ -356,4 +506,3 @@ L.KMLMarker = L.Marker.extend({
 		icon: new L.KMLIcon.Default()
 	}
 });
-
