7b763ab7cc5365b12924e6ada9fdb02f3d3ccb1b
Jonatan Machado <jonatan.mac@gmail.com>
2014-10-15 16:00:10 -0300

    Fixing KML rotation

--- layer/vector/KML.js
+++ layer/vector/KML.js
@@ -417,10 +417,10 @@ L.RotatedImageOverlay = L.ImageOverlay.extend({
 	_rotate: function () {
         if (L.DomUtil.TRANSFORM) {
             // use the CSS transform rule if available
-            this._image.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
+            this._image.style[L.DomUtil.TRANSFORM] += ' rotate(' + -this.options.angle + 'deg)';
         } else if(L.Browser.ie) {
             // fallback for IE6, IE7, IE8
-            var rad = this.options.angle * (Math.PI / 180),
+            var rad = -this.options.angle * (Math.PI / 180),
                 costheta = Math.cos(rad),
                 sintheta = Math.sin(rad);
             this._image.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' + 
