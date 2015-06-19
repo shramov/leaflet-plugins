33420acabcbf0de96b968f8ebc53f4dfe17183b1
brunob <brunobergot@gmail.com>
2015-06-19 19:52:10 +0200

    Fix #150 : protocol relativity for Bing layer

--- layer/tile/Bing.js
+++ layer/tile/Bing.js
@@ -49,8 +49,10 @@ L.BingLayer = L.TileLayer.extend({
 			}
 			_this.initMetadata();
 		};
-		var url = document.location.protocol + '//dev.virtualearth.net/REST/v1/Imagery/Metadata/' + this.options.type + '?include=ImageryProviders&jsonp=' + cbid +
-		          '&key=' + this._key + '&UriScheme=' + document.location.protocol.slice(0, -1);
+		var urlScheme = (document.location.protocol === 'file:') ? 'http' : document.location.protocol.slice(0, -1);
+		var url = urlScheme + '://dev.virtualearth.net/REST/v1/Imagery/Metadata/'
+					+ this.options.type + '?include=ImageryProviders&jsonp=' + cbid +
+					'&key=' + this._key + '&UriScheme=' + urlScheme;
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = url;
