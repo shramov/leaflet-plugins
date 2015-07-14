cd7002096db7536fe94123a156bb5f50831292e6
brunob <brunobergot@gmail.com>
2015-07-14 14:37:28 +0200

    amend #168 : space/tabs, and many jshints fixes

--- layer/vector/KML.js
+++ layer/vector/KML.js
@@ -18,15 +18,33 @@ L.KML = L.FeatureGroup.extend({
 		if (options === undefined) options = this.options;
 
 		var req = new window.XMLHttpRequest();
-		req.open('GET', url, async);
-		try {
-			req.overrideMimeType('text/xml'); // unsupported by IE
-		} catch(e) {}
-		req.onreadystatechange = function() {
-			if (req.readyState !== 4) return;
-			if (req.status === 200) cb(req.responseXML, options);
-		};
-		req.send(null);
+		
+		// Check for IE8 and IE9 Fix Cors for those browsers
+		if (req.withCredentials === undefined && typeof window.XDomainRequest !== 'undefined') {
+			var xdr = new window.XDomainRequest();
+			xdr.open('GET', url, async);
+			xdr.onprogress = function () { };
+			xdr.ontimeout = function () { };
+			xdr.onerror = function () { };
+			xdr.onload = function () {
+				if (xdr.responseText) {
+					var xml = new window.ActiveXObject('Microsoft.XMLDOM');
+					xml.loadXML(xdr.responseText);
+					cb(xml, options);
+				}
+			};
+			setTimeout(function () { xdr.send(); }, 0);
+		} else {
+			req.open('GET', url, async);
+			try {
+				req.overrideMimeType('text/xml'); // unsupported by IE
+			} catch (e) { }
+			req.onreadystatechange = function () {
+				if (req.readyState !== 4) return;
+				if (req.status === 200) cb(req.responseXML, options);
+			};
+			req.send(null);
+		}
 	},
 
 	addKML: function(url, options, async) {
