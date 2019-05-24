92bd6419783196b43325779c4ae58eee3359b703
johndoe <johnd0e@mail.ua>
2019-05-24 15:40:10 +0300

    L.Yandex: remove unused stuff
    * L.Layer is already inherited from L.Evented
      As for L.Mixin.Events, then we do not support Leaflet 0.x
    
    * actual max available zoom level is 19
    
    * _insertAtBottom, _limitedUpdate: was not used
    
    * getAttribution is already in L.Layer
    
    * _reset contains _initContainer, which already called in onAdd
       And as we already have container, it makes no sense to check it on 'viewreset'
    
    * setOpacity: no need to expose it
    
    * setElementSize: is excessive, as css '100%' is to follow map resize.
    
    * undefined is same as false in conditions
    
    * consistent curly braces usage and other minor style issues

--- layer/tile/Yandex.js
+++ layer/tile/Yandex.js
@@ -1,18 +1,11 @@
-/*
- * L.TileLayer is used for standard xyz-numbered tile layers.
- */
-
 /* global ymaps: true */
 
 L.Yandex = L.Layer.extend({
-	includes: L.Evented ? L.Evented.prototype : L.Mixin.Events,
 
 	options: {
+		traffic: false,
 		minZoom: 0,
-		maxZoom: 18,
-		attribution: '',
-		opacity: 1,
-		traffic: false
+		maxZoom: 19
 	},
 
 	possibleShortMapTypes: {
@@ -23,7 +16,7 @@ L.Yandex = L.Layer.extend({
 		publicMapInHybridView: 'publicMapHybrid',
 		overlay: 'overlay'
 	},
-	
+
 	_getPossibleMapType: function (mapType) {
 		var result = 'yandex#map';
 		if (typeof mapType !== 'string') {
@@ -40,7 +33,7 @@ L.Yandex = L.Layer.extend({
 		}
 		return result;
 	},
-	
+
 	// Possible types: yandex#map, yandex#satellite, yandex#hybrid, yandex#publicMap, yandex#publicMapHybrid
 	// Or their short names: map, satellite, hybrid, publicMap, publicMapHybrid
 	initialize: function (type, options) {
@@ -53,52 +46,19 @@ L.Yandex = L.Layer.extend({
 		this._type = this._getPossibleMapType(type || this.options.type);
 	},
 
-	onAdd: function (map, insertAtTheBottom) {
-		this._map = map;
-		this._insertAtTheBottom = insertAtTheBottom;
-
-		// create a container div for tiles
+	onAdd: function () {
 		this._initContainer();
 		this._initMapObject();
-
-		// set up events
-		map.on('viewreset', this._reset, this);
-
-		this._limitedUpdate = L.Util.throttle(this._update, 150, this);
 		map.on('move', this._update, this);
-
 		map._controlCorners.bottomright.style.marginBottom = '3em';
-
-		this._reset();
-		this._update(true);
 	},
 
 	onRemove: function (map) {
 		this._container.remove();
-
-		this._map.off('viewreset', this._reset, this);
-
 		this._map.off('move', this._update, this);
-
 		if (map._controlCorners) {
 			map._controlCorners.bottomright.style.marginBottom = '0em';
 		}
-	},
-
-	getAttribution: function () {
-		return this.options.attribution;
-	},
-
-	setOpacity: function (opacity) {
-		this.options.opacity = opacity;
-		if (opacity < 1) {
-			L.DomUtil.setOpacity(this._container, opacity);
-		}
-	},
-
-	setElementSize: function (e, size) {
-		e.style.width = size.x + 'px';
-		e.style.height = size.y + 'px';
 	},
 
 	_initContainer: function () {
@@ -107,33 +67,40 @@ L.Yandex = L.Layer.extend({
 				+ (this.options.overlay ? 'leaflet-overlay-pane' : 'leaflet-tile-pane');
 			this._container = L.DomUtil.create('div', className);
 			this._container.id = '_YMapContainer_' + L.Util.stamp(this);
-			this.setOpacity(this.options.opacity);
+			if (this.options.opacity < 1) {
+				L.DomUtil.setOpacity(this._container, this.options.opacity);
+			}
+			this._container.style.width = '100%';
+			this._container.style.height = '100%';
 		}
 		this._map.getContainer().appendChild(this._container);
-		this.setElementSize(this._container, this._map.getSize());
 	},
 
 	_initMapObject: function () {
-		if (this._yandex) return;
+		if (this._yandex) {
+			this._update();
+			return;
+		}
 
 		// Check that ymaps.Map is ready
-		if (ymaps.Map === undefined) {
+		if (!ymaps.Map) {
 			return ymaps.load(['package.map'], this._initMapObject, this);
 		}
 
 		// If traffic layer is requested check if control.TrafficControl is ready
-		if (this.options.traffic)
-			if (ymaps.control === undefined ||
-					ymaps.control.TrafficControl === undefined) {
+		if (this.options.traffic) {
+			if (!ymaps.control || !ymaps.control.TrafficControl) {
 				return ymaps.load(['package.traffic', 'package.controls'],
 					this._initMapObject, this);
 			}
+		}
+
 		//Creating ymaps map-object without any default controls on it
 		var map = new ymaps.Map(this._container, {center: [0, 0], zoom: 0, behaviors: [], controls: []});
 
-		if (this.options.traffic)
+		if (this.options.traffic) {
 			map.controls.add(new ymaps.control.TrafficControl({shown: true}));
-
+		}
 		if (this.options.overlay) {
 			this._type = new ymaps.MapType('overlay', []);
 			map.container.getElement().style.background = 'transparent';
@@ -141,31 +108,16 @@ L.Yandex = L.Layer.extend({
 		map.setType(this._type);
 
 		this._yandex = map;
-		this._update(true);
-		
+		this._update();
+
 		//Reporting that map-object was initialized
-		this.fire('MapObjectInitialized', {mapObject: map});
+		this.fire('MapObjectInitialized', { mapObject: map });
 	},
 
-	_reset: function () {
-		this._initContainer();
-	},
-
-	_update: function (force) {
-		if (!this._yandex) return;
-		this._resize(force);
-
+	_update: function () {
+		if (!this._yandex) { return; }
 		var center = this._map.getCenter();
-		var zoom = this._map.getZoom();
-		this._yandex.setCenter([center.lat, center.lng], zoom);
-	},
-
-	_resize: function (force) {
-		var size = this._map.getSize(), style = this._container.style;
-		if (style.width === size.x + 'px' && style.height === size.y + 'px')
-			if (force !== true) return;
-		this.setElementSize(this._container, size);
-		this._yandex.container.fitToViewport();
+		this._yandex.setCenter([center.lat, center.lng], this._map.getZoom());
 	}
 });
 
